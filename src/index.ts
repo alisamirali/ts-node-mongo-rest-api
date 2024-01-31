881eb07408ff45742d9eaac604984462c15557e8
Ali Samir <alisamir.eng@gmail.com>
2024-01-31 15:35:38 +0200

    dev

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
 });
