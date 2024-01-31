4bc4d8f5e1ea8124d60c277c6693e99b1c339f7e
Ali Samir <alisamir.eng@gmail.com>
2024-01-31 14:59:15 +0200

    dev

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+require("dotenv").config();
+
+import router from "./router";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+});
+
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("connected", () => console.log("Connected to MongoDB"));
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/api", router);
